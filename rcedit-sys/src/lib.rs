a2a1b89453f18fa72ff41a04ebf7f9def6f594ee
Tim Fish <tim@timfish.uk>
2022-01-29 13:06:58 +0000

    Expose versions and execution level

--- rcedit-sys/src/lib.rs
+++ rcedit-sys/src/lib.rs
@@ -12,6 +12,39 @@ extern "C" {
     pub fn ResourceUpdater_Commit(ctx: ResourceUpdater) -> bool;
 
     pub fn ResourceUpdater_SetIcon(ctx: ResourceUpdater, w_icon_path: *const u16) -> bool;
-    pub fn ResourceUpdater_ChangeRcdata(ctx: ResourceUpdater, id: u32, w_rcdata_path: *const u16) -> bool;
-    pub fn ResourceUpdater_ChangeString(ctx: ResourceUpdater, id: u32, w_rcdata_path: *const u16) -> bool;
+
+    pub fn ResourceUpdater_SetVersionString(
+        ctx: ResourceUpdater,
+        w_name: *const u16,
+        w_value: *const u16,
+    ) -> bool;
+
+    pub fn ResourceUpdater_SetProductVersion(
+        ctx: ResourceUpdater,
+        v1: u16,
+        v2: u16,
+        v3: u16,
+        v4: u16,
+    ) -> bool;
+
+    pub fn ResourceUpdater_SetFileVersion(
+        ctx: ResourceUpdater,
+        v1: u16,
+        v2: u16,
+        v3: u16,
+        v4: u16,
+    ) -> bool;
+
+    pub fn ResourceUpdater_SetExecutionLevel(ctx: ResourceUpdater, wLevel: *const u16) -> bool;
+
+    pub fn ResourceUpdater_ChangeRcdata(
+        ctx: ResourceUpdater,
+        id: u32,
+        w_rcdata_path: *const u16,
+    ) -> bool;
+    pub fn ResourceUpdater_ChangeString(
+        ctx: ResourceUpdater,
+        id: u32,
+        w_rcdata_path: *const u16,
+    ) -> bool;
 }
